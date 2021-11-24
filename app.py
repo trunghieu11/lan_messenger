b692ae02d6c362a4a41cf3dda1c1aae3e870b1f3
trunghieu11 <vuondenthanhcong11@gmail.com>
2021-11-24 20:20:07 +0700

    read config from environ

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import utils
+import os
 
 from bot import Bot
 from flask import Flask, request
@@ -9,40 +10,18 @@ config = utils.read_ini("config.ini")
 CONFIG_SECTION = "PROD"
 
 FB_API_URL = 'https://graph.facebook.com/v12.0/me/messages'
-VERIFY_TOKEN = config[CONFIG_SECTION]["VERIFY_TOKEN"]
-PAGE_ACCESS_TOKEN = config[CONFIG_SECTION]["PAGE_ACCESS_TOKEN"]
+# VERIFY_TOKEN = config[CONFIG_SECTION]["VERIFY_TOKEN"]
+# PAGE_ACCESS_TOKEN = config[CONFIG_SECTION]["PAGE_ACCESS_TOKEN"]
+VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
+PAGE_ACCESS_TOKEN = os.environ["PAGE_ACCESS_TOKEN"]
 
 lan = Bot(access_token=PAGE_ACCESS_TOKEN, api_version="12.0")
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-def get_bot_response(message):
-    """This is just a dummy function, returning a variation of what
-    the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
-
 
 def verify_webhook(req):
     if req.args.get("hub.verify_token") == VERIFY_TOKEN:
         return req.args.get("hub.challenge")
     else:
         return "incorrect"
-
-def respond(sender, message):
-    """Formulate a response to the user and
-    pass it on to a function that sends it."""
-    response = get_bot_response(message)
-    send_message(sender, response)
-
-
-def is_user_message(message):
-    """Check if the message is a message from the user"""
-    return (message.get('message') and
-            message['message'].get('text') and
-            not message['message'].get("is_echo"))
-
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def listen():
@@ -52,14 +31,6 @@ def listen():
         return verify_webhook(request)
 
     if request.method == 'POST':
-        # payload = request.json
-        # event = payload['entry'][0]['messaging']
-        # for x in event:
-        #     if is_user_message(x):
-        #         text = x['message']['text']
-        #         sender_id = x['sender']['id']
-        #         respond(sender_id, text)
-        
         output = request.get_json()
         
         for event in output['entry']:
@@ -81,41 +52,3 @@ def listen():
                     pass
 
         return "ok"
-
-def send_message(recipient_id, text):
-    """Send a response to Facebook"""
-    payload = {
-        'message': {
-            'text': text,
-            "quick_replies":[
-                {
-                    "content_type": "text",
-                    "title":"Title option_1",
-                    "payload":"option_1",
-                    # "image_url":"http://example.com/img/red.png"
-                },
-                {
-                    "content_type": "text",
-                    "title":"Title option_2",
-                    "payload":"option_2",
-                    # "image_url":"http://example.com/img/green.png"
-                }
-            ],
-        },
-        'recipient': {
-            'id': recipient_id
-        },
-        'notification_type': 'regular'
-    }
-
-    auth = {
-        'access_token': PAGE_ACCESS_TOKEN
-    }
-
-    response = requests.post(
-        FB_API_URL,
-        params=auth,
-        json=payload
-    )
-
-    return response.json()
